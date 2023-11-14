eba7864407b3d0ffec2b9b203969184ce4c89c03
Ellen Irene <141398828+EllenIrene@users.noreply.github.com>
2023-11-14 12:55:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Datatyper_undervisning
+    1 referaanse
+{ 
+    int tall = 1;
+
+float desimalTall = 1.01F; //nøyaktig
+doble desimalTall2 = 1.01;
+decimal desimalTall3 = 1.01M; // veldig nøyaktig
+
+char bokstav = 'a';
+string tekst = "Hei";
+
+bool santUsant = true; // eller false
+
+int[] tallArray = { 1, 2, 3 };
+string[] tekstArray = { "Hei", "på", "deg" };
+
+List<int> ListeMedTall = new List<int>(); // Lager en ny liste med tall.
+
+public int NumberFive()
+{
+
+Return  5;
+
+
+PublicKey string NumberFive()
+    {
+
+        return "five";
+
+    } }
+}
